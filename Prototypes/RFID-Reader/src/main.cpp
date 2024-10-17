c9540cdb68036e00650324fcfe331b7ec87eb56a
Youmayu <103046178+Youmayu@users.noreply.github.com>
2024-10-17 15:36:51 +0200

    Cleanup

--- Prototypes/RFID-Reader/src/main.cpp
+++ Prototypes/RFID-Reader/src/main.cpp
@@ -1,26 +1,3 @@
-/*
- * Initial Author: ryand1011 (https://github.com/ryand1011)
- *
- * Reads data written by a program such as "rfid_write_personal_data.ino"
- *
- * See: https://github.com/miguelbalboa/rfid/tree/master/examples/rfid_write_personal_data
- *
- * Uses MIFARE RFID card using RFID-RC522 reader
- * Uses MFRC522 - Library
- * -----------------------------------------------------------------------------------------
- *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
- *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
- * Signal      Pin          Pin           Pin       Pin        Pin              Pin
- * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
- * SPI SS      SDA(SS)      10            53        D10        10               10
- * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
- * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
- * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
- *
- * More pin layouts for other boards can be found here: https://github.com/miguelbalboa/rfid#pin-layout
-*/
-
 #include <Arduino.h>
 #include <SPI.h>
 #include <MFRC522.h>
