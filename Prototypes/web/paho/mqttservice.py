8b4b974017f42f030457ee320f9337b6a9b48481
wltwt <wltwt@proton.me>
2024-11-06 14:02:13 +0100

    Setup logging for pymqtt

--- Prototypes/web/paho/mqttservice.py
+++ Prototypes/web/paho/mqttservice.py
@@ -2,31 +2,43 @@ import paho.mqtt.client as paho
 import requests
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv()
+
+log_dir = "/var/log/mqttclient"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(level=logging.INFO, filename=f"{log_dir}/mqtt.log",filemode="w")
+logger = logging.getLogger(__name__)
 
 username = os.getenv("MQTT_ADMIN_USERNAME")
 password = os.getenv("MQTT_ADMIN_PASSWORD")
 
+logger.info(f"got username {username}")
 
-def on_subscribe(client, userdata, mid, granted_qos):
-    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+def on_subscribe(client, userdata, flags, rc):
+    if rc == 0:
+        logger.info("Connected to broker")
+        client.subscribe('data', qos=1)
+    else:
+        logger.error(f"Failed due to: {rc}")
+
 
 def on_message(client, userdata, msg):
-    #print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload)) 
+    logger.info(msg.topic+" "+str(msg.qos)+" "+str(msg.payload)) 
     payload = {
         'topic': msg.topic,
         'message': msg.payload.decode()
     }
     try:
-        requests.post("127.0.0.1:8000/mqtt", json=payload)
+        requests.post("http://127.0.0.1:8000/mqtt", json=payload)
     except requests.exceptions.RequestException as e:
-        print(e)
+        logger.error(e)
 
 client = paho.Client()
 client.on_connect = on_subscribe
 client.on_message = on_message
 client.username_pw_set(username, password)
 client.connect("mqtt.gruppe1.tech", 9001)
-client.subscribe('encyclopedia/#', qos=1)
+client.subscribe('data', qos=1)
 client.loop_forever()
