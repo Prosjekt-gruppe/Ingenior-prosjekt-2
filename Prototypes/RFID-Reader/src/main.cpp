cf398e192ed556daf16ff7f8b34806611fce1b3b
Youmayu <103046178+Youmayu@users.noreply.github.com>
2024-11-15 01:02:28 +0100

    Noted where I left off

--- Prototypes/RFID-Reader/src/main.cpp
+++ Prototypes/RFID-Reader/src/main.cpp
@@ -87,6 +87,8 @@ void loop() {
   //Update the mqtt
   mqtt.update();
 
+//WORK IN PROGRESS HERE!!!
+
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return;
