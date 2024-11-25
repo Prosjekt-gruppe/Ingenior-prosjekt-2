8f66bca3e725feee491d111adae09ac2d7a49ed2
wltwt <wltwt@proton.me>
2024-11-25 20:28:35 +0100

    More logging

--- Prototypes/web/app/__init__.py
+++ Prototypes/web/app/__init__.py
@@ -1,17 +1,27 @@
 import os
 from flask import Flask, send_from_directory
 from flask_socketio import SocketIO
+from utils.logging import logger
 
 socketio = SocketIO(cors_allowed_origins="*")
 
 def create_app():
+    logger.info("Creating app...")
+    
     app = Flask(__name__)
+    logger.info("Started app successfully")
 
     from . import front, mqtt
-    app.register_blueprint(front.bp)
-    app.register_blueprint(mqtt.bp)
+    logger.info("Imported fron and mqtt successfully")
     
+
+    app.register_blueprint(front.bp)
+    logger.info("Registered front successfully")
+    
+    app.register_blueprint(mqtt.bp)
+    logger.info("Regustered mqtt successfully")
     
     socketio.init_app(app)
+    logger.info("Started SocketIO successfully")
 
     return app
