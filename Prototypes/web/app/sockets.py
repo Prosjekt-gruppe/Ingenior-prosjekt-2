3199d626152129ce8ac6b455ba722ecfc4892c75
wltwt <wltwt@proton.me>
2024-11-27 03:37:05 +0100

    Try fixing sockets

--- Prototypes/web/app/sockets.py
+++ Prototypes/web/app/sockets.py
@@ -4,6 +4,7 @@ from utils.logging import logger
 @socketio.on('connect')
 def handle_connect():
     logger.info("Client connected")
+    socketio.emit('server_message', {'data': 'Welcome!'})
 
 @socketio.on('disconnect')
 def handle_disconnect():
